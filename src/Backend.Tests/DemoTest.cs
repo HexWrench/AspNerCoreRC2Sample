15ae722cbe88b4e4fe5c92762da3ba45ab3568a2
j.ahnen <j.ahnen@speedikonfm.com>
2016-05-18 14:56:42 +0200

    initial

---
+++ src/Backend.Tests/DemoTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+using MyApp.Backend;
+using Xunit;
+
+
+namespace BackendTests
+{
+   public class DemoTest
+   {
+      [Fact]
+      public void My()
+      {
+         Demo c = new Demo();
+         bool contains = c.ContainsFive(new List<int> { 1, 3, 5 });
+         Assert.True(contains);
+      }
+
+      [Fact]
+      public void GetText()
+      {
+         var demoMock = new Mock<IDemo>();
+         demoMock.Setup(dm => dm.GetText()).Returns("Test");
+
+         string text = demoMock.Object.GetText();
+         Assert.Equal("Test", text);
+      }
+
+      [Fact]
+      public void PassingTest()
+      {
+         Assert.Equal(4, Add(2, 2));
+      }
+
+      [Fact]
+      public void FailingTest()
+      {
+         Assert.Equal(5, Add(2, 2));
+      }
+
+      int Add(int x, int y)
+      {
+         return x + y;
+      }
+   }
+}
---
+++ src/Backend/Demo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApp.Backend
+{
+    public class Demo
+    {
+        public bool ContainsFive(List<int> l)
+        {
+            return l.Any(i => i == 5);
+        }
+
+       public string GetText(IDemo demo)
+       {
+          return demo.GetText();
+       }
+    }
+
+   public interface IDemo
+   {
+      string GetText();
+   }
+}
